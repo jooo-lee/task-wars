5c31c6ba6af5ed3350d8ae68824bdd82dc05777c
Joseph Lee <josephlee3004@gmail.com>
2024-01-01 14:27:03 -0500

    Create Todo class

---
+++ src/js/todo.js
@@ -0,0 +1,29 @@
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.complete = false;
+    }
+
+    updateTitle(newTitle) {
+        this.title = newTitle;
+    }
+
+    updateDescription(newDescription) {
+        this.description = newDescription;
+    }
+
+    updateDueDate(newDueDate) {
+        this.dueDate = newDueDate;
+    }
+
+    updatePriority(newPriority) {
+        this.priority = newPriority;
+    }
+
+    updateCompletion() {
+        this.complete = !this.complete;
+    }
+}
