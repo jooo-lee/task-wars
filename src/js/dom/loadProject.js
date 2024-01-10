254e5914801be67b85d9b32d3f046cb6f7785f4e
Joseph Lee <josephlee3004@gmail.com>
2024-01-10 14:31:34 -0500

    Create view todo details button

--- src/js/dom/loadProject.js
+++ src/js/dom/loadProject.js
@@ -12,6 +12,49 @@ function createDeleteTodoBtn(todo, project) {
     return deleteTodoBtn;
 }
 
+function closeModal(modal) {
+    modal.close();
+    modal.remove();
+    document.querySelector("body").style.overflow = "auto"; // Allow for scrolling after modal is closed
+}
+
+function createViewTodoDetailsBtn(todo) {
+    const todoDetailsBtn = document.createElement("button");
+    todoDetailsBtn.textContent = "View details";
+    todoDetailsBtn.addEventListener("click", function () {
+        const modal = document.createElement("dialog");
+
+        const closeBtn = document.createElement("button");
+        closeBtn.textContent = "Close";
+        closeBtn.addEventListener("click", function () {
+            closeModal(modal);
+        });
+        modal.appendChild(closeBtn);
+
+        const p = createParagraph("Todo details");
+        modal.appendChild(p);
+
+        // Close modal when clicking outside of it
+        modal.addEventListener("click", function (e) {
+            const modalDimensions = modal.getBoundingClientRect();
+            if (
+                e.clientX < modalDimensions.left ||
+                e.clientX > modalDimensions.right ||
+                e.clientY < modalDimensions.top ||
+                e.clientY > modalDimensions.bottom
+            ) {
+                closeModal(modal);
+            }
+        });
+
+        const body = document.querySelector("body");
+        body.appendChild(modal);
+        body.style.overflow = "hidden"; // Prevent scrolling of background
+        modal.showModal();
+    });
+    return todoDetailsBtn;
+}
+
 function createTodoListItem(todo, project) {
     const li = document.createElement("li");
 
@@ -20,6 +63,9 @@ function createTodoListItem(todo, project) {
 
     const todoDueDate = createParagraph(todo.dueDate);
     li.appendChild(todoDueDate);
+
+    const todoDetailsBtn = createViewTodoDetailsBtn(todo);
+    li.appendChild(todoDetailsBtn);
 
     const deleteTodoBtn = createDeleteTodoBtn(todo, project);
     li.appendChild(deleteTodoBtn);
