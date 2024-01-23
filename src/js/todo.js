51ee85eb75aef9742edc1a97865ed165b8edf76f
Joseph Lee <josephlee3004@gmail.com>
2024-01-23 09:39:45 -0500

    Remove unnecessary methods of Todo class

--- src/js/todo.js
+++ src/js/todo.js
@@ -8,26 +8,6 @@ class Todo {
         this.priority = priority;
         this.complete = false;
     }
-
-    updateTitle(newTitle) {
-        this.title = newTitle;
-    }
-
-    updateDescription(newDescription) {
-        this.description = newDescription;
-    }
-
-    updateDueDate(newDueDate) {
-        this.dueDate = newDueDate;
-    }
-
-    updatePriority(newPriority) {
-        this.priority = newPriority;
-    }
-
-    updateCompletion() {
-        this.complete = !this.complete;
-    }
 }
 
 export default Todo;
