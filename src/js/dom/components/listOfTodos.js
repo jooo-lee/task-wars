83eace5f302ee638fc0283bace8529f4e4b4c8e2
Joseph Lee <josephlee3004@gmail.com>
2024-01-28 12:26:40 -0500

    Make list of todos an unordered list

--- src/js/dom/components/listOfTodos.js
+++ src/js/dom/components/listOfTodos.js
@@ -1,8 +1,8 @@
 import createTodoListItem from "./todoListItem";
 
-// Here, list refers to the <ol> tag
+// Here, list refers to the <ul> tag
 function createListOfTodos(project) {
-    const listOfTodos = document.createElement("ol");
+    const listOfTodos = document.createElement("ul");
     listOfTodos.id = "list-of-todos";
     for (const todo of project.todos) {
         const todoListItem = createTodoListItem(todo, project);
