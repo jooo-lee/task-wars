c9c2f282b9cb9d0dc6a2bb4ff51d1b7dd8c8ae9d
Joseph Lee <josephlee3004@gmail.com>
2024-01-31 14:22:02 -0500

    Add deleteProject method to User class

--- src/js/user.js
+++ src/js/user.js
@@ -8,6 +8,18 @@ class User {
     addProject(project) {
         this.#projects.push(project);
     }
+
+    deleteProject(targetProject) {
+        const projectToBeDeleted = this.#projects.find(
+            (project) => project.uuid === targetProject.uuid
+        );
+        const indexOfProjectToBeDeleted =
+            this.#projects.indexOf(projectToBeDeleted);
+
+        if (indexOfProjectToBeDeleted > -1) {
+            this.#projects.splice(indexOfProjectToBeDeleted, 1);
+        }
+    }
 }
 
 export default User;
