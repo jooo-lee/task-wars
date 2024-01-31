62827296006061dcc28f25a51d59e8e69c008842
Joseph Lee <josephlee3004@gmail.com>
2024-01-31 14:20:16 -0500

    Add uuid to each project for better identification during deletion

--- src/js/project.js
+++ src/js/project.js
@@ -1,7 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
+
 class Project {
     #todos;
 
     constructor(title) {
+        this.uuid = uuidv4();
         this.title = title;
         this.#todos = [];
     }
